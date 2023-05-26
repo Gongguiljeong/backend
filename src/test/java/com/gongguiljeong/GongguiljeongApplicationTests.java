fc66cfca5cd5ceb54ba97c61b9836ed484fa9650
rldh1111 <rldh11111@naver.com>
2023-05-26 18:16:05 +0900

    Feat. 초기세팅

---
+++ src/test/java/com/gongguiljeong/GongguiljeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongguiljeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongguiljeongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
