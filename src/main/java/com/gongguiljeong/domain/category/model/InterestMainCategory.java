62d3563a8afb94c0d016b5b2eb66560d3e6e0d19
rldh1111 <rldh11111@naver.com>
2023-05-27 14:35:45 +0900

    [Feat] Entity 세팅

---
+++ src/main/java/com/gongguiljeong/domain/category/model/InterestMainCategory.java
@@ -0,0 +1,28 @@
+package com.gongguiljeong.domain.category.model;
+
+import com.gongguiljeong.domain.category.model.MainCategory;
+import com.gongguiljeong.domain.user.model.User;
+import com.gongguiljeong.global.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class InterestMainCategory extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "interest_main_category_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "main_category_id", nullable = false)
+    private MainCategory mainCategory;
+}
---
+++ src/main/java/com/gongguiljeong/domain/interest_gongguiljeong/model/InterestGongguiljeong.java
@@ -0,0 +1,30 @@
+package com.gongguiljeong.domain.interest_gongguiljeong.model;
+
+import com.gongguiljeong.domain.gongguiljeong.model.Gongguiljeong;
+import com.gongguiljeong.domain.user.model.User;
+import com.gongguiljeong.global.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class InterestGongguiljeong extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "interest_gongguiljeong_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false )
+    private User user;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gongguiljeong_id", nullable = false)
+    private Gongguiljeong gongguiljeong;
+
+}
