3c97f379db7ec4d926a552b4b54e5fe455cfba4b
kwongio <rldh11111@naver.com>
2023-05-28 16:43:41 +0900

    [Feat] EnableJpaAuditing 뺌

--- src/main/java/com/gongguiljeong/GongguiljeongApplication.java
+++ src/main/java/com/gongguiljeong/GongguiljeongApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class GongguiljeongApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GongguiljeongApplication.class, args);
     }
