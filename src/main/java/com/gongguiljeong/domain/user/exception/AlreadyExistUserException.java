7a0b62a9eb77084c7c05d29e080be79cab7b7416
kwongio <rldh11111@naver.com>
2023-05-30 17:02:56 +0900

    [Feat] AlreadyExistUserException 추가

---
+++ src/main/java/com/gongguiljeong/domain/user/exception/AlreadyExistUserException.java
@@ -0,0 +1,12 @@
+package com.gongguiljeong.domain.user.exception;
+
+
+
+
+public class AlreadyExistUserException extends RuntimeException {
+    private final static String MESSAGE = "이미 존재하는 회원입니다.";
+
+    public AlreadyExistUserException() {
+        super(MESSAGE);
+    }
+}
