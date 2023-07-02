f19750392a6086337d7cffd7ea3ac91372ca1777
kwongio <rldh11111@naver.com>
2023-07-02 23:19:54 +0900

    [Refactor]: Succuess Response 삭제

--- src/main/java/com/gongguiljeong/global/util/SecurityResponse.java
+++ src/main/java/com/gongguiljeong/global/util/SecurityResponse.java
@@ -8,27 +8,22 @@ import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 
+import static com.gongguiljeong.domain.common.domain.exception.ExceptionCode.*;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
 public class SecurityResponse {
 
     //로그인 안함
     public static void unAuthentication(HttpServletResponse response) throws IOException {
-        String result = getResponse(ExceptionCode.UN_AUTHENTICATION);
+        String result = getResponse(UN_AUTHENTICATION);
         response.setContentType("application/json; charset=utf-8");
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.getWriter().println(result);
-    }
-
-    // 로그인 성공했을 때
-    public static void success(HttpServletResponse response, Object dto) throws IOException {
-        String result = getResponse(ExceptionCode.SUCCESS);
-        response.setContentType("application/json; charset=utf-8");
-        response.setStatus(200);
+        response.setStatus(UNAUTHORIZED.value());
         response.getWriter().println(result);
     }
 
     //권한없는 페이지에 들어갈때
     public static void forbidden(HttpServletResponse response) throws IOException {
-        String result = getResponse(ExceptionCode.FORBIDDEN);
+        String result = getResponse(FORBIDDEN);
         response.setContentType("application/json; charset=utf-8");
         response.setStatus(HttpStatus.FORBIDDEN.value());
         response.getWriter().println(result);
