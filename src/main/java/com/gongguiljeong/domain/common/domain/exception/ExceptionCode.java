bed7d15675bb134d5516345c8dc46f85ff3dc8e6
kwongio <rldh11111@naver.com>
2023-07-02 23:24:09 +0900

    [Chore]:  ExceptionCode 로그인, 권한 처리 에러코드 변경

--- src/main/java/com/gongguiljeong/domain/common/domain/exception/ExceptionCode.java
+++ src/main/java/com/gongguiljeong/domain/common/domain/exception/ExceptionCode.java
@@ -4,18 +4,17 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @Getter
 public enum ExceptionCode {
 
     //Common
-    SUCCESS(HttpStatus.OK.value(), HttpStatus.OK, "성공"),
-    UN_AUTHENTICATION(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN, "권한이 없습니다."),
-    NOT_FOUND(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, "찾을 수 없습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다."),
-
+    UN_AUTHENTICATION(0001, HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
+    FORBIDDEN(0002, HttpStatus.FORBIDDEN, "권한이 없습니다."),
+    NOT_FOUND(0003, HttpStatus.NOT_FOUND, "찾을 수 없습니다."),
+    BAD_REQUEST(0004, HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(0005, HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다."),
 
     //User
     USER_NOT_FOUND(1001, HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
@@ -48,7 +47,6 @@ public enum ExceptionCode {
 
     INTEREST_GONGGUILJEONG_NOT_FOUND(8001, HttpStatus.NOT_FOUND, "관심공구일정을 찾을 수 없습니다."),
     INTEREST_GONGGUILJEONG_ALREADY_EXIST(8002, HttpStatus.BAD_REQUEST, "이미 존재하는 관심공구일정입니다.");
-
 
 
     private final int code;
